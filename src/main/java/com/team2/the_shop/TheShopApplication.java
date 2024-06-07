7a7dda4023a45bfa38e4fa0f7129f5fd890e57cf
Jamie Blomerus <jamie.blomerus@protonmail.com>
2024-06-07 18:47:42 +0200

    Initial commit

---
+++ src/main/java/com/team2/the_shop/TheShopApplication.java
@@ -0,0 +1,13 @@
+package com.team2.the_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheShopApplication.class, args);
+	}
+
+}
