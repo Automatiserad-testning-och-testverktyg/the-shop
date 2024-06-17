e493b632ed4c59174f1f96381e6a147bed11a09c
LiBe0413 <linn.bergstrom@iths.se>
2024-06-17 18:36:21 +0200

    Update src/test/java/com/team2/the_shop/stepDef_homepage.java
    Co-authored-by: Jamie Blomerus <34009701+jamieblomerus@users.noreply.github.com>

--- src/test/java/com/team2/the_shop/stepDef_homepage.java
+++ src/test/java/com/team2/the_shop/stepDef_homepage.java
@@ -7,7 +7,6 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
