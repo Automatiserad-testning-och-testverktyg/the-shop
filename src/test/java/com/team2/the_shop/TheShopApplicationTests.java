57100e53f258889e897f753d07ed5da2c9570898
CardonaCervantes <144700510+CardonaCervantes@users.noreply.github.com>
2024-06-18 18:41:32 +0200

    Added glue parameter and junit-vintage to pom.xml

--- src/test/java/com/team2/the_shop/TheShopApplicationTests.java
+++ src/test/java/com/team2/the_shop/TheShopApplicationTests.java
@@ -1,13 +1,17 @@
 package com.team2.the_shop;
 
-import org.junit.jupiter.api.Test;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "src/test/java/com/team2/the_shop/resource/features",
+		plugin = {"pretty", "html:target/cucumber-report.html"},
+		glue={"com.team2.the_shop.stepDef_SmokeTest"}
+		)
+
 @SpringBootTest
-class TheShopApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+public class TheShopApplicationTests {
 }
