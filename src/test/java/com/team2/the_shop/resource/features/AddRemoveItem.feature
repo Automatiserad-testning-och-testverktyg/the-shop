5e6ddd652775d4e6fec6a3ebec53a9e1dc37c658
CardonaCervantes <sebastian.cardonacervantes@firefly.se>
2024-06-20 15:38:23 +0200

    Not great tests. Also a subset of Jamie's tests in checkout.feature. Albeit written differently. Could be expanded, methods could be written to be more flexible, but not at this moment. Please review. Feature 2 is too long.

--- src/test/java/com/team2/the_shop/resource/features/AddRemoveItem.feature
+++ src/test/java/com/team2/the_shop/resource/features/AddRemoveItem.feature
@@ -1,12 +1,14 @@
 Feature: AddRemoveItem
 
   Scenario: Test add 1 item to the cart
-    Given the user is on the page "https://webshop-agil-testautomatiserare.netlify.app/products.html"
-    When  the user adds an item to the cart
+    Given the user is on the page "https://webshop-agil-testautomatiserare.netlify.app/products"
+    When  the user adds 1 item to the cart
     Then  1 item should be added to the cart
 
   Scenario: Test remove 1 item from the cart
-    Given the user is on the page "https://webshop-agil-testautomatiserare.netlify.app/checkout"
+    Given the user is on the page "https://webshop-agil-testautomatiserare.netlify.app/products"
+    When  the user adds 1 item to the cart
+    And   the user goes to the page "https://webshop-agil-testautomatiserare.netlify.app/checkout"
     And   the cart contains 1 item
-    When  the user removes 1 item from the cart
+    And   the user removes 1 item from the cart
     Then  the cart should contain 0 items
--- src/test/java/com/team2/the_shop/stepDef_AddRemoveItem.java
+++ src/test/java/com/team2/the_shop/stepDef_AddRemoveItem.java
@@ -1,10 +1,7 @@
 package com.team2.the_shop;
 
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+import io.cucumber.java.*;
+import io.cucumber.java.en.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -14,74 +11,75 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class stepDef_AddRemoveItem { // Skrivet av Sebastian Cardona Cervantes
 
-    WebDriver driver;
-    //WebDriverWait wait;
+    private WebDriver driver;
+    private WebDriverWait wait;
 
     @Before
     public void setup() {
         ChromeOptions options = new ChromeOptions();
-        //options.addArguments("--incognito");
+        options.addArguments("--incognito");
         options.addArguments("--headless=new");
-        options.addArguments("--disable-gpu");
         driver = new ChromeDriver(options);
-        //driver.manage().window().maximize();
-        //wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(1));
     }
 
     @After
     public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
+        driver.quit();
     }
 
     @Given("the user is on the page {string}")
-    public void the_user_is_on_the_page(String url) {
+    public void theUserIsOnThePage(String url) {
+        driver.get(url);
+    }
+    @And("the user goes to the page {string}")
+    public void theUserGoesToThePage(String url) {
         driver.get(url);
     }
 
-    @When("the user adds an item to the cart")
-    public void the_user_adds_an_item_to_the_cart() {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement addToCartButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#main > div:nth-child(14) > div > div > button")));
-        //visibilityOfElementLocated(By.cssSelector(".col:nth-child(14) .btn")));
+    @When("the user adds {int} item to the cart")
+    public void theUserAddsAnItemToTheCart(int noOfItems ) {
+        List<WebElement> productItems = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//*[@id=\"main\"]/div[" + noOfItems + "]/div/div/button")));
+        WebElement addToCartButton = productItems.getFirst();
         addToCartButton.click();
     }
 
     @Then("{int} item should be added to the cart")
-    public void item_should_be_added_to_the_cart(int expectedSize) {
-        WebElement buttonSize = driver.findElement(By.id("buttonSize"));
-        String buttonSizeValue = buttonSize.getAttribute("value");
+    public void itemShouldBeAddedToTheCart(int expectedSize) {
+        WebElement buttonSize = wait.until(ExpectedConditions.elementToBeClickable(By.id("buttonSize")));
+        String buttonSizeValue = buttonSize.getText();
         int actualSize = Integer.parseInt(buttonSizeValue);
-        assertEquals(expectedSize, actualSize);
+        assertEquals(expectedSize, actualSize, "The number of items added to the cart is not correct.");
     }
+
 
     @Given("the cart contains {int} item")
-    public void the_cart_contains_item(int expectedSize) {
-        WebElement buttonSize = driver.findElement(By.id("buttonSize"));
-        String buttonSizeValue = buttonSize.getAttribute("value");
+    public void theCartContainsItem(int expectedSize) {
+        WebElement buttonSize = wait.until(ExpectedConditions.elementToBeClickable(By.id("buttonSize")));
+        String buttonSizeValue = buttonSize.getText();
         int actualSize = Integer.parseInt(buttonSizeValue);
-        assertEquals(expectedSize, actualSize);
+        assertEquals(expectedSize, actualSize, "The number of items in the cart is not correct.");
     }
 
-    @When("the user removes 1 item from the cart")
-    public void the_user_removes_item_from_the_cart() {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement addToCartButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#cartList > li.list-group-item.d-flex.justify-content-between.lh-sm > div > button")));
+    @When("the user removes {int} item from the cart")
+    public void theUserRemoves1ItemFromTheCart(int noOfItems) {
+        List<WebElement> removeButton = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//*[@id=\"cartList\"]/li[" + noOfItems + "]/div/button")));
+        WebElement addToCartButton = removeButton.getFirst();
         addToCartButton.click();
     }
 
     @Then("the cart should contain {int} items")
-    public void the_cart_should_contain_items(int expectedSize) {
+    public void theCartShouldContainItems(int expectedSize) {
         WebElement buttonSize = driver.findElement(By.id("buttonSize"));
-        String buttonSizeValue = buttonSize.getAttribute("value");
+        String buttonSizeValue = buttonSize.getText();
         int actualSize = Integer.parseInt(buttonSizeValue);
-        assertEquals(expectedSize, actualSize);
+        assertEquals(expectedSize, actualSize, "The number of items in the cart is not correct.");
     }
-
 }
